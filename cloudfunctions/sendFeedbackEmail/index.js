65aef70b69bafeec1b6ea4b3f7e94e7ccf1a9c32
张轩铭 <2300990296@qq.com>
2025-04-24 16:04:36 +0800

    202504241604

--- cloudfunctions/sendFeedbackEmail/index.js
+++ cloudfunctions/sendFeedbackEmail/index.js
@@ -1,6 +1,7 @@
 'use strict'
 exports.main = async (event) => {
   const nodemailer = require('nodemailer')
+  const validator = require('validator')
   const mailerconfig = {
     host: 'smtp.qq.com',
     secure: true,
@@ -33,18 +34,39 @@ exports.main = async (event) => {
         errFix: '传递有效的product参数'
       }
     }
-    if (typeof (requestdata.content) != 'string') {
+    if (typeof (requestdata.content) != 'string' || !requestdata.content) {
       return {
         errCode: 1001,
         errMsg: '请求参数错误',
         errFix: '传递有效的content参数'
       }
     }
+    let contacttype = '未知'
+    let contactvalue = '未知'
+    const validcontacttypes = ['邮箱', '微信', 'QQ']
+    if (validcontacttypes.includes(requestdata.contactType)) {
+      if (typeof (requestdata.contactValue) != 'string' || !requestdata.contactValue) {
+        return {
+          errCode: 1001,
+          errMsg: '请求参数错误',
+          errFix: '传递有效的contactValue参数'
+        }
+      }
+      if (requestdata.contactType == '邮箱' && !validator.isEmail(requestdata.contactValue)) {
+        return {
+          errCode: 1001,
+          errMsg: '请求参数错误',
+          errFix: '传递有效的contactValue参数'
+        }
+      }
+      contacttype = requestdata.contactType
+      contactvalue = requestdata.contactValue
+    }
     await nodemailer.createTransport(mailerconfig).sendMail({
       from: 'zhangls2512@vip.qq.com',
       to: '2300990296@qq.com',
       subject: '有新反馈',
-      text: '产品：' + requestdata.product + '\n内容：' + requestdata.content
+      text: '产品：' + requestdata.product + '\n内容：' + requestdata.content + '\n联系方式：' + contactvalue + '（' + contacttype + '）'
     })
     return {
       errCode: 0,
