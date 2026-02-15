b3d072f3f864a04512119ce8297e1ac742fa22c5
张轩铭 <2300990296@qq.com>
2026-02-16 00:09:50 +0800

    202602160009

--- src/product/updatelog/webdavhelper.vue
+++ src/product/updatelog/webdavhelper.vue
@@ -2,6 +2,20 @@
 document.title = '轩铭2512 - 更新日志 - WebDAV 助手'
 const data = [
   {
+    versionNumber: '1.0.0.2',
+    releaseDate: '2026-02-15',
+    logs: [
+      {
+        type: 'F',
+        content: '长按（鼠标右键）删除单个文件或目录成功后未刷新“文件”页面'
+      },
+      {
+        type: 'F',
+        content: '我的 - 设置 - 通用 - 通用 - 更多 - 应用启动后拉起切换账号弹窗未生效'
+      }
+    ]
+  },
+  {
     versionNumber: '1.0.0.1',
     releaseDate: '2026-02-14',
     logs: [
