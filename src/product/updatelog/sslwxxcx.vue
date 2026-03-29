041b9ffd3e3d38484506c3dc066c6a43161f4232
张轩铭 <2300990296@qq.com>
2026-03-29 13:35:12 +0800

    202603291335

--- src/product/updatelog/sslwxxcx.vue
+++ src/product/updatelog/sslwxxcx.vue
@@ -2,6 +2,16 @@
 document.title = '轩铭2512 - 更新日志 - SSL 证书（微信小程序）'
 const data = [
   {
+    versionNumber: '2.0.20',
+    releaseDate: '2026-03-29',
+    logs: [
+      {
+        type: 'F',
+        content: '修复了一些已知的问题'
+      }
+    ]
+  },
+  {
     versionNumber: '2.0.19',
     releaseDate: '2026-03-19',
     logs: [
