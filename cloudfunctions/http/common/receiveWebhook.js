b249f6caa0731cbaa98ef06c64f54b5dd40626e0
张轩铭 <2300990296@qq.com>
2026-04-27 13:54:25 +0800

    202604271354

--- cloudfunctions/http/common/receiveWebhook.js
+++ cloudfunctions/http/common/receiveWebhook.js
@@ -45,7 +45,6 @@ exports.main = async (event) => {
     }
   }
   let channelname = ''
-  let url = ''
   const resourceid = receivedata.resourceId
   const validresourceids = ['0e7893fb67e67aec0012ffd02eae679a', 'b013194767e67b6600146b805c3a6ba5', '80a8bd4f67e67bb6001344e3625c0400', '9f86c65667e67c2f00130c9f5b343245']
   if (!validresourceids.includes(resourceid)) {
@@ -57,26 +56,22 @@ exports.main = async (event) => {
   }
   if (resourceid == '0e7893fb67e67aec0012ffd02eae679a') {
     channelname = 'Canary'
-    url = 'https://aka.ms/canaryLatest'
   }
   if (resourceid == 'b013194767e67b6600146b805c3a6ba5') {
     channelname = 'Dev'
-    url = 'https://aka.ms/devLatest'
   }
   if (resourceid == '80a8bd4f67e67bb6001344e3625c0400') {
     channelname = 'Beta'
-    url = 'https://aka.ms/betaLatest'
   }
   if (resourceid == '9f86c65667e67c2f00130c9f5b343245') {
     channelname = 'Releasepreview'
-    url = 'https://aka.ms/releasepreviewLatest'
   }
   try {
     const { data } = await axios.post('http://api.chuckfang.com:12580/subscribe/openBroadcast', channelname + '频道有新版本：' + receivedata.newVersion + '。', {
       params: {
         channelId: 'dfe6398536a54e1dad34928d6b812944',
         unionId: process.env.unionid,
-        url: url
+        url: 'https://learn.microsoft.com/en-us/windows-insider/about'
       },
       headers: {
         'Content-Type': 'text/plain'
