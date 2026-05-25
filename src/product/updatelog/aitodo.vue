7d44308a48b48687046907f18fda39e038ca22ff
张轩铭 <2300990296@qq.com>
2026-05-25 23:18:22 +0800

    202605252318

--- src/product/updatelog/aitodo.vue
+++ src/product/updatelog/aitodo.vue
@@ -2,6 +2,48 @@
 document.title = '轩铭2512 - 更新日志 - 智能待办'
 const data = [
   {
+    versionNumber: '1.0.0.1',
+    releaseDate: '2026-05-25',
+    logs: [
+      {
+        type: 'A',
+        content: '我的 - 设置 - 云备份 新增 自动导入（登录轩铭 2512 统一账号时可见）'
+      },
+      {
+        type: 'A',
+        content: '我的 - 设置 - 通用 - 通用 - 界面 - 深色模式 新增 跟随环境光（仅搭载环境光传感器的设备可用）'
+      },
+      {
+        type: 'A',
+        content: '我的 - 设置 - 通用 - 通用 - 功能 新增 桌面应用图标角标显示可完成待办数量'
+      },
+      {
+        type: 'A',
+        content: '调试 - 设置 - 基础 新增 提示系统进行 GC、转储当前线程虚拟机原始堆快照'
+      },
+      {
+        type: 'A',
+        content: '“推荐待办”卡片新增 2*2 规格，可在设备待机屏保界面上显示'
+      },
+      {
+        type: 'U',
+        content: '“待办”支持鼠标框选多选、已过结束时间待办的内容显示为红色'
+      },
+      {
+        type: 'U',
+        content: '部分场景适配键盘快捷键'
+      },
+      {
+        type: 'U',
+        content: '卡片开启模糊背板'
+      },
+      {
+        type: 'F',
+        content: 'CalDAV 备份多个待办时可能失败'
+      }
+    ]
+  },
+  {
     versionNumber: '1.0.0.0',
     releaseDate: '2026-05-18',
     logs: [
